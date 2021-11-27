d72d9e5afa9fa909f5ded39d2179f0ca4318652f
Hello <hello@mail.com>
2021-11-27 10:06:36 +0545

    Fixed imports

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
-const app = express;
+var app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
+    schema,
   })
 );
 
